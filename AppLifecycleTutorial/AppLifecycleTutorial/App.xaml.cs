76dd95d59261a23d19e25aa4e9cd4611cd4c8d74
SahnoAnton <83997566+anton4534@users.noreply.github.com>
2023-12-21 14:42:21 +0200

    Add files via upload

---
+++ AppLifecycleTutorial/AppLifecycleTutorial/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AppLifecycleTutorial
+{
+    public partial class App : Application
+    {
+        const string displayText = "displayText";
+        public string DisplayText { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+        
+        protected override void OnStart() /*при запуске приложения*/
+        {
+            Console.WriteLine("Стартовало приложение то");
+
+            //Затем при запуске приложения, при условии, 
+            //что словарь Properties содержит ключ displayText, 
+            //значение ключа восстанавливается в свойство DisplayText.
+
+
+
+            if (Properties.ContainsKey(displayText))
+            {
+                DisplayText = (string)Properties[displayText];
+            }
+        }
+
+        protected override void OnSleep()/*когда приложение в фоновом режиме*/
+        {
+            //Когда приложение находится в фоновом режиме или завершает работу, 
+            //метод OnSleep переопределяет значение свойства DisplayText словарю Properties 
+            //ключом displayText.
+
+            Console.WriteLine("Спим");
+            Properties[displayText] = DisplayText;
+        }
+
+        protected override void OnResume()/*когда приложение выходит из фонового режима*/
+        {
+            Console.WriteLine("Проснулись");
+        }
+    }
+}
---
+++ AppLifecycleTutorial/AppLifecycleTutorial/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AppLifecycleTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        
+        
+        }
+
+        protected override void OnAppearing()
+        {
+            //Метод OnAppearing возвращает значение свойства 
+            //App.DisplayText и задает его как значение свойства TextEntry.
+
+
+            base.OnAppearing();
+
+            entry.Text = (Application.Current as App).DisplayText;
+
+            //Переопределение метода OnAppearing выполняется после готовности ContentPage, 
+            //но только прежде чем он станет видимым.Таким образом лучше всего 
+            //    задать содержимое представлений Xamarin.Forms.
+        }
+
+        //Когда текст в Entry завершен с помощью ключа возврата, 
+        //в свойстве App.DisplayTextвыполняется метод 
+        //    OnEntryCompleted и хранится текст Entry.
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            (Application.Current as App).DisplayText = entry.Text;
+        }
+    }
+}
